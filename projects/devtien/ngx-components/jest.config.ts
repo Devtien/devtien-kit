e3e4a8e20cbd8d89e49bce384c4ecd5efe2a4c50
Martijn Hoogstraten <martijn@devtien.nl>
2024-02-19 00:17:20 +0100

    Added jest support

---
+++ projects/devtien/ngx-components/jest.config.ts
@@ -0,0 +1,19 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/src/test.ts',
+  ],
+};
+
+export default config;

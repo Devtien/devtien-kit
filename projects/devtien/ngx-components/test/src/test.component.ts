9b633960249d2f695b79393af0df22792ba427fb
Martijn Hoogstraten <martijn@devtien.nl>
2024-02-25 19:57:19 +0100

    Added test component

---
+++ projects/devtien/ngx-components/test/src/test.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { TableComponent } from '@devtien/ngx-components/table';
+
+@Component({
+  selector: 'tien-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.css'
+})
+export class TestComponent {
+  db = [
+    {
+      id: 132,
+      options: {
+        component: TableComponent.name,
+      }
+    }
+  ];
+  constructor() {
+
+  }
+
+  getDBHTML() {
+    return `
+<div data-uhce='1'>
+
+</div>
+    `;
+  }
+}
